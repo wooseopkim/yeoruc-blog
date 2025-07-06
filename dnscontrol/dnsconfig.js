2df40dfd2ef913ee71f984b2b42a003e11201b6c
Wooseop Kim <wooseop@protonmail.com>
2025-07-06 11:23:42 +0900

    Use triple-slash directive instead of `import`

--- dnscontrol/dnsconfig.js
+++ dnscontrol/dnsconfig.js
@@ -1,14 +1,6 @@
 // @ts-check
-
-import "./types-dnscontrol";
+/// <reference types="./types-dnscontrol" />
 
 D("yeoruc.blog", NewRegistrar("none"), DnsProvider(NewDnsProvider("porkbun")),
-    A("@", "185.199.108.153"),
-    A("@", "185.199.109.153"),
-    A("@", "185.199.110.153"),
-    A("@", "185.199.111.153"),
-    AAAA("@", "2606:50c0:8000::153"),
-    AAAA("@", "2606:50c0:8001::153"),
-    AAAA("@", "2606:50c0:8002::153"),
-    AAAA("@", "2606:50c0:8003::153"),
+    A("@", "216.198.79.1"),
 );
